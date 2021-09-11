a89f2d321e8b230741e28a0d9be9aceaa45ed845
Petr Šádek <shetr.git@gmail.com>
2021-09-11 21:02:14 +0200

    renaming local storage for user data + removing initial user

--- src/js/control/app.js
+++ src/js/control/app.js
@@ -34,20 +34,10 @@ class App
         this.router = new Router(routes, defaultRoute);
         this.router.route();
 
-        this._users = new AppStorage("users", [User, CustomDate, Note, DiaryNote]);
+        this._users = new AppStorage("diary-js-users", [User, CustomDate, Note, DiaryNote]);
         this._users.load();
 
         this.user = null;
-
-        if(this._users.data.length < 1) {
-            User.createWithPassword("petr@email.cz", "heslo").then((user) => {
-                let note1 = DiaryNote.createDiaryNote(CustomDate.createDate(6,6,2021),"kaj semestralka","odevzdat");
-                let note2 = DiaryNote.createDiaryNote(CustomDate.createDate(21,6,2021),"zubar","vycistit zuby");
-                user.diaryNotes = [note1, note2];
-                this._users.data.push(user);
-                this._users.save();
-            });
-        }
     }
 
     getUsers() {
